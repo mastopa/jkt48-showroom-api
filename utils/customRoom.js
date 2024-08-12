2a8af755cb5501ab4be9555b6de718ffc91dabe9
MasThopa <55644837+mastopa@users.noreply.github.com>
2024-08-13 00:36:41 +0700

    Update customRoom.js

--- utils/customRoom.js
+++ utils/customRoom.js
@@ -32,32 +32,12 @@ const getCustomRoom = (type) => {
   } else {
     rooms = {
       amanda: "400710",
-      christy: "318112",
       lia: "400713",
-      zee: "317727",
       callie: "400714",
-      oniel: "318218",
-      olla: "318218",
-      feni: "317738",
-      fiony: "318223",
-      flora: "318224",
-      freya: "318225",
-      ella: "400715",
-      gita: "318117",
-      gracie: "461478",
-      greesel: "461479",
-      eli: "318118",
-      indah: "318227",
+      ela: "400715",
       indira: "400716",
-      jessi: "318228",
       lyn: "400717",
-      kathrina: "318230",
-      lulu: "318232",
-      marsha: "318233",
-      muthe: "318204",
       raisha: "400718",
-      adel: "318239",
-      gracia: "318208",
     };
   }
   return rooms;
